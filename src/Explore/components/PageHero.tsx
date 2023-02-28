2ff55ed2cf937f5f64dbf7bf6d38b1aef3793af3
Nworie Kingsley <obizzkingsley@gmail.com>
2023-02-28 11:28:26 +0100

    complete the hero UI

--- src/Explore/components/PageHero.tsx
+++ src/Explore/components/PageHero.tsx
@@ -1,14 +1,24 @@
 import "../styles/pageHero.css";
+import video from "../assets/videos/hero.mp4";
 
 function PageHero() {
   return (
     <div className="page-hero">
+      <div className="video-wrap">
+        <video autoPlay muted loop id="heroVideo">
+          <source src={video} type="video/mp4" />
+          Sorry, your browser doesn't support videos.
+        </video>
+      </div>
+
       <div className="text-wrap">
-        <h3>Explore the biggest book collections in Nigeria</h3>
-        <p>
-          Millions of writers and publishers around the world showcase their
-          work on BookClub - the home to the world’s best books.
-        </p>
+        <div>
+          <h3>Explore the biggest book collections in Nigeria</h3>
+          <p>
+            Millions of writers and publishers around the world showcase their
+            work on BookClub - the home to the world’s best books.
+          </p>
+        </div>
       </div>
 
       <div className="search-box-wrap">
--- src/Explore/index.tsx
+++ src/Explore/index.tsx
@@ -1,12 +1,45 @@
 import Navbar from "../common/components/Navbar";
 import PageHero from "./components/PageHero";
-import "./styles/pageHero.css";
+import bookCover from "./assets/book-cover.png";
+import "./styles/bookGrid.css";
 
 function Explore() {
   return (
     <div>
       <Navbar />
       <PageHero />
+      <div className="booklist-section">
+        <ol className="booklist-wrapper">
+          <li>
+            <img src={bookCover} alt="" />
+            <div className="action-bar">
+              <div className="action-status">Want to Read</div>
+              <button className="action-dropdown">^</button>
+            </div>
+          </li>
+          <li>
+            <img src={bookCover} alt="" />
+          </li>
+          <li>
+            <img src={bookCover} alt="" />
+          </li>
+          <li>
+            <img src={bookCover} alt="" />
+          </li>
+          <li>
+            <img src={bookCover} alt="" />
+          </li>
+          <li>
+            <img src={bookCover} alt="" />
+          </li>
+          <li>
+            <img src={bookCover} alt="" />
+          </li>
+          <li>
+            <img src={bookCover} alt="" />
+          </li>
+        </ol>
+      </div>
     </div>
   );
 }
